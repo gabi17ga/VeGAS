cdb09e90e464761ffb1ac713b0b455fa645b424d
Cristina Gabriela Vasile <gabi17ga@gmail.com>
2025-12-18 09:33:54 +0200

    Enhance run script by adding container name and fixing output volume path indentation for clarity.

--- run.sh
+++ run.sh
@@ -4,15 +4,17 @@
 # Use -v to mount current dir, -e to pass environment variables, and place the image
 # name before the command to execute inside the container.
 docker run --rm \
+  --name vegas \
   --platform linux/amd64 \
   -v "$(pwd)":/vegas \
+  -v /Volumes/vegas/output:/vegas/data/output \
   -w /vegas \
   -e HOST_GENOME=/vegas/data/host/ \
   -e REFERENCE_GENOME=/vegas/data/reference/ \
   snakemake/snakemake \
   python src/main.py \
     -d /vegas/data/input/ \
-  -o /vegas/data/output/ \
+    -o /vegas/data/output/ \
     -r /vegas/data/reference/ \
     -t /vegas/data/host/ \
     -c 1 -cc 1 --overwrite
